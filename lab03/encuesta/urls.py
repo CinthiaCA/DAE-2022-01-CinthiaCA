48dad89ed207a6b34846b1ebd33bf77cc80e8971
CinthiaCA <cinthia.calla@tecsup.edu.pe>
2022-04-03 22:47:50 -0500

    Lab03

---
+++ lab03/encuesta/urls.py
@@ -0,0 +1,19 @@
+from unicodedata import name
+from urllib.parse import urlparse
+from django.urls import URLPattern, path
+
+from . import views
+app_name = 'encuesta'
+
+urlpatterns= [
+    #ex: /encuesta/
+    path('', views.index, name='index'),
+    #ex: /encuesta/5/
+    path('<int:pregunta_id>/', views.detalle, name='detalle'),
+    #ex: /encuesta/5/resultados/
+    path('<int:pregunta_id>/resultados/', views.resultados, name='resultados'),
+    #ex: /encuesta/5/voto/
+    path('<int:pregunta_id>/voto/', views.votar, name='votar'),
+
+
+]
