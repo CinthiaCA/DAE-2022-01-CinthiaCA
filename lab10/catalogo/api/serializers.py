7547eb77a694cb62a5b07294f40ea0f0942174ac
CinthiaCA <cinthia.calla@tecsup.edu.pe>
2022-05-20 03:05:04 -0500

    Lab10

---
+++ lab10/catalogo/api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Producto
+
+
+class ProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Producto
+        fields = ('id', 'nombre', 'precio')
+
+    
+
+    
+
---
+++ lab10/lab10/src/App.js
@@ -0,0 +1,43 @@
+import {Component} from 'react'
+import Layout from './components/Layout';
+import Productos from './components/Productos';
+import Title from './components/Title';
+import Navbar from './components/Navbar';
+
+class App extends Component{
+
+  state ={
+    productos:[]
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      productos: []
+    }
+}
+
+  componentDidMount() {
+    fetch('http://localhost:8000/productos')
+      .then((response) => {
+        return response.json()
+      })
+      .then((prod) => {
+        this.setState({ productos: prod })
+      })
+  } 
+
+   render(){
+     return(
+       <div>
+          <Navbar></Navbar>
+         <Layout>
+           <Title></Title>
+
+           <Productos productos={this.state.productos}/>
+         </Layout>
+       </div>
+     )
+   }
+}
+export default App;
